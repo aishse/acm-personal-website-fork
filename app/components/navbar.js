f5f2cf60b4e0abe04ea640acd032a9771029ea05
Anishka Chauhan <email.friendme@gmail.com>
2025-10-14 15:55:13 -0700

    ps2

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -1,9 +1,9 @@
 export default function Navbar() {
   return (
-    <nav className="flex gap-4 font-semibold text-xl py-4">
-      <a href="#">Home</a>
-      <a href="#about" >About Me</a>
-      <a href="#projects" >Projects</a>
+    <nav className="flex gap-8 font-semibold text-xl py-4 fixed bg-black w-full top-0 px-8">
+      <a href="#" className="hover:scale-105 hover:text-gray-300">Home</a>
+      <a href="#about"  className="hover:scale-105 hover:text-gray-300">About Me</a>
+      <a href="#projects" className="hover:scale-105 hover:text-gray-300" >Projects</a>
     </nav>
   )
 }
--- app/components/projects.js
+++ app/components/projects.js
@@ -2,27 +2,32 @@ import Image from 'next/image'
 
 export default function Projects() {
   return (
-    <div id="projects">
+    <div id="projects" className='scroll-mt-20'>
       <h2 className="text-4xl pt-8 pb-4 font-bold">Projects</h2>
       <div className="grid grid-cols-2 gap-12">
-        <div className="rounded-xl">
-          <h3 className="text-2xl font-semibold pb-4">Destructor-Inator Ray</h3>
+
+        <div className="rounded-xl bg-purple-600 justify-items-center text-center py-5">
+          <h3 className="text-2xl font-semibold pb-4 ">Destructor-Inator Ray</h3>
           <Image src="/destructor-inator.jpg" alt="Destructor-Inator Ray" className="rounded-xl pb-2" width={400} height={300} />
           <p className="pt-4 pb-2">An inator that can destroy any object in the tri-state area.</p>
-          <a href="#">Learn More</a>
+          <a href="#" className="font-bold text-xl hover:text-black transition-colors duration-350 ">Learn More</a>
         </div>
-        <div className="rounded-xl">
+
+        <div className="rounded-xl bg-purple-600 justify-items-center text-center py-5">
           <h3 className="text-2xl font-semibold pb-4">De-Love-inator</h3>
           <Image src="/de-love-inator.png" alt="De-Love-inator" className="rounded-xl pb-2" width={400} height={300} />
           <p className="pt-4 pb-2">A low-earth-orbit inator that emits a laser that eliminates love from the Tri-State area.</p>
-          <a href="#">Learn More</a>
+          <a href="#"  className="font-bold text-xl hover:text-black transition-colors duration-350 ">Learn More</a>
         </div>
-        <div className="rounded-xl">
+
+        <div className="rounded-xl bg-purple-600 justify-items-center text-center py-5">
           <h3 className="text-2xl font-semibold pb-4">Make-Everything-Evil-inator</h3>
           <Image src="/evil-inator.png" alt="Make-Everything-Evil-inator" className="rounded-xl pb-2" width={400} height={300} />
           <p className="pt-4 pb-2">An inator that emits a ray that turns anything evil.</p>
-          <a href="#">Learn More</a>
+          <a href="#"  className="font-bold text-xl hover:text-black transition-colors duration-350 ">Learn More</a>
         </div>
+
+
       </div>
 
     </div>
